868ac569a11d26410cedcd47f9f2a7e703647358
Kernel Yang <zeus83157@gmail.com>
2022-08-03 00:51:17 +0800

    add component.

---
+++ chatroom_app/src/components/appForm.vue
@@ -0,0 +1,23 @@
+<template>
+    <form class="form">
+        <formTextbox style="margin-bottom: 10px;" />
+        <input type="button" @click="btnLogin_Click(this)" class="btn btn-lg btn-primary btn-block" value="Submit" />
+    </form>
+</template>
+
+<style lang="css" src="@/assets/css/Form.css">
+</style>
+
+<script>
+import { defineComponent } from "vue";
+import formTextbox from "@/components/formTextbox.vue";
+export default defineComponent({
+    components: {
+        formTextbox
+    },
+    methods: {
+        btnLogin_Click: function () {
+        }
+    }
+})
+</script>
---
+++ chatroom_app/src/components/formTextbox.vue
@@ -0,0 +1,9 @@
+<template>
+    <input name="password" type="password" class="form-control" placeholder="Password">
+</template>
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+})
+</script>
