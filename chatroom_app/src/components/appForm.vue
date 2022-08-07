b353347893fc0e5d7312449afb3672734c9aa83d
Kernel Yang <zeus83157@gmail.com>
2022-08-07 19:30:20 +0800

    Adjust css.

--- chatroom_app/src/components/appForm.vue
+++ chatroom_app/src/components/appForm.vue
@@ -12,7 +12,28 @@
 
 <style lang="css" src="@/assets/css/Form.css">
 </style>
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+}
 
+nav {
+    padding: 30px;
+}
+
+nav a {
+    font-weight: bold;
+    color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+    color: #42b983;
+}
+</style>
 <script>
 import { defineComponent } from "vue";
 import formTextbox from "@/components/formTextbox.vue";
